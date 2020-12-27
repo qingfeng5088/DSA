ecb4f3b72d52b298683024ffa5c437a8a2f115c9
qingfeng5088 <75205706+qingfeng5088@users.noreply.github.com>
2020-12-27 19:59:35 +0800

    AVL 树和红黑树性能比较

---
+++ src/BinaryTree/AVLTree.java
@@ -0,0 +1,556 @@
+package BinaryTree;
+
+import java.util.Arrays;
+
+/**
+ * Java 语言: AVL树
+ */
+
+public class AVLTree<T extends Comparable<T>> {
+    private AVLTreeNode<T> mRoot;    // 根结点
+
+    // AVL树的节点(内部类)
+    class AVLTreeNode<T extends Comparable<T>> {
+        T key;                // 关键字(键值)
+        int height;         // 高度
+        AVLTreeNode<T> left;    // 左孩子
+        AVLTreeNode<T> right;    // 右孩子
+
+        public AVLTreeNode(T key, AVLTreeNode<T> left, AVLTreeNode<T> right) {
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.height = 0;
+        }
+    }
+
+    // 构造函数
+    public AVLTree() {
+        mRoot = null;
+    }
+
+    /*
+     * 获取树的高度
+     */
+    private int height(AVLTreeNode<T> tree) {
+        if (tree != null)
+            return tree.height;
+
+        return 0;
+    }
+
+    public int height() {
+        return height(mRoot);
+    }
+
+    /*
+     * 比较两个值的大小
+     */
+    private int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    /*
+     * 前序遍历"AVL树"
+     */
+    private void preOrder(AVLTreeNode<T> tree) {
+        if (tree != null) {
+            System.out.print(tree.key + " ");
+            preOrder(tree.left);
+            preOrder(tree.right);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(mRoot);
+    }
+
+    /*
+     * 中序遍历"AVL树"
+     */
+    private void inOrder(AVLTreeNode<T> tree) {
+        if (tree != null) {
+            inOrder(tree.left);
+            System.out.print(tree.key + " ");
+            inOrder(tree.right);
+        }
+    }
+
+    public void inOrder() {
+        inOrder(mRoot);
+    }
+
+    /*
+     * 后序遍历"AVL树"
+     */
+    private void postOrder(AVLTreeNode<T> tree) {
+        if (tree != null) {
+            postOrder(tree.left);
+            postOrder(tree.right);
+            System.out.print(tree.key + " ");
+        }
+    }
+
+    public void postOrder() {
+        postOrder(mRoot);
+    }
+
+    /*
+     * (递归实现)查找"AVL树x"中键值为key的节点
+     */
+    private AVLTreeNode<T> search(AVLTreeNode<T> x, T key) {
+        if (x == null)
+            return x;
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            return search(x.left, key);
+        else if (cmp > 0)
+            return search(x.right, key);
+        else
+            return x;
+    }
+
+    public AVLTreeNode<T> search(T key) {
+        return search(mRoot, key);
+    }
+
+    /*
+     * (非递归实现)查找"AVL树x"中键值为key的节点
+     */
+    private AVLTreeNode<T> iterativeSearch(AVLTreeNode<T> x, T key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0)
+                x = x.left;
+            else if (cmp > 0)
+                x = x.right;
+            else
+                return x;
+        }
+
+        return x;
+    }
+
+    public AVLTreeNode<T> iterativeSearch(T key) {
+        return iterativeSearch(mRoot, key);
+    }
+
+    /*
+     * 查找最小结点：返回tree为根结点的AVL树的最小结点。
+     */
+    private AVLTreeNode<T> minimum(AVLTreeNode<T> tree) {
+        if (tree == null)
+            return null;
+
+        while (tree.left != null)
+            tree = tree.left;
+        return tree;
+    }
+
+    public T minimum() {
+        AVLTreeNode<T> p = minimum(mRoot);
+        if (p != null)
+            return p.key;
+
+        return null;
+    }
+
+    /*
+     * 查找最大结点：返回tree为根结点的AVL树的最大结点。
+     */
+    private AVLTreeNode<T> maximum(AVLTreeNode<T> tree) {
+        if (tree == null)
+            return null;
+
+        while (tree.right != null)
+            tree = tree.right;
+        return tree;
+    }
+
+    public T maximum() {
+        AVLTreeNode<T> p = maximum(mRoot);
+        if (p != null)
+            return p.key;
+
+        return null;
+    }
+
+    /*
+     * LL：左左对应的情况(左单旋转)。
+     *
+     * 返回值：旋转后的根节点
+     */
+    private AVLTreeNode<T> leftLeftRotation(AVLTreeNode<T> k2) {
+        AVLTreeNode<T> k1;
+
+        k1 = k2.left;
+        k2.left = k1.right;
+        k1.right = k2;
+
+        k2.height = max(height(k2.left), height(k2.right)) + 1;
+        k1.height = max(height(k1.left), k2.height) + 1;
+
+        return k1;
+    }
+
+    /*
+     * RR：右右对应的情况(右单旋转)。
+     *
+     * 返回值：旋转后的根节点
+     */
+    private AVLTreeNode<T> rightRightRotation(AVLTreeNode<T> k1) {
+        AVLTreeNode<T> k2;
+
+        k2 = k1.right;
+        k1.right = k2.left;
+        k2.left = k1;
+
+        k1.height = max(height(k1.left), height(k1.right)) + 1;
+        k2.height = max(height(k2.right), k1.height) + 1;
+
+        return k2;
+    }
+
+    /*
+     * LR：左右对应的情况(左双旋转)。
+     *
+     * 返回值：旋转后的根节点
+     */
+    private AVLTreeNode<T> leftRightRotation(AVLTreeNode<T> k3) {
+        k3.left = rightRightRotation(k3.left);
+
+        return leftLeftRotation(k3);
+    }
+
+    /*
+     * RL：右左对应的情况(右双旋转)。
+     *
+     * 返回值：旋转后的根节点
+     */
+    private AVLTreeNode<T> rightLeftRotation(AVLTreeNode<T> k1) {
+        k1.right = leftLeftRotation(k1.right);
+
+        return rightRightRotation(k1);
+    }
+
+    /*
+     * 将结点插入到AVL树中，并返回根节点
+     *
+     * 参数说明：
+     *     tree AVL树的根结点
+     *     key 插入的结点的键值
+     * 返回值：
+     *     根节点
+     */
+    private AVLTreeNode<T> insert(AVLTreeNode<T> tree, T key) {
+        if (tree == null) {
+            // 新建节点
+            tree = new AVLTreeNode<T>(key, null, null);
+            if (tree == null) {
+                System.out.println("ERROR: create avltree node failed!");
+                return null;
+            }
+        } else {
+            int cmp = key.compareTo(tree.key);
+
+            if (cmp < 0) {    // 应该将key插入到"tree的左子树"的情况
+                tree.left = insert(tree.left, key);
+                // 插入节点后，若AVL树失去平衡，则进行相应的调节。
+                if (height(tree.left) - height(tree.right) == 2) {
+                    if (key.compareTo(tree.left.key) < 0)
+                        tree = leftLeftRotation(tree);
+                    else
+                        tree = leftRightRotation(tree);
+                }
+            } else if (cmp > 0) {    // 应该将key插入到"tree的右子树"的情况
+                tree.right = insert(tree.right, key);
+                // 插入节点后，若AVL树失去平衡，则进行相应的调节。
+                if (height(tree.right) - height(tree.left) == 2) {
+                    if (key.compareTo(tree.right.key) > 0)
+                        tree = rightRightRotation(tree);
+                    else
+                        tree = rightLeftRotation(tree);
+                }
+            } else {    // cmp==0
+                System.out.println("添加失败：不允许添加相同的节点！");
+            }
+        }
+
+        tree.height = max(height(tree.left), height(tree.right)) + 1;
+
+        return tree;
+    }
+
+    public void insert(T key) {
+        mRoot = insert(mRoot, key);
+    }
+
+    /*
+     * 删除结点(z)，返回根节点
+     *
+     * 参数说明：
+     *     tree AVL树的根结点
+     *     z 待删除的结点
+     * 返回值：
+     *     根节点
+     */
+    private AVLTreeNode<T> remove(AVLTreeNode<T> tree, AVLTreeNode<T> z) {
+        // 根为空 或者 没有要删除的节点，直接返回null。
+        if (tree == null || z == null)
+            return null;
+
+        int cmp = z.key.compareTo(tree.key);
+        if (cmp < 0) {        // 待删除的节点在"tree的左子树"中
+            tree.left = remove(tree.left, z);
+            // 删除节点后，若AVL树失去平衡，则进行相应的调节。
+            if (height(tree.right) - height(tree.left) == 2) {
+                AVLTreeNode<T> r = tree.right;
+                if (height(r.left) > height(r.right))
+                    tree = rightLeftRotation(tree);
+                else
+                    tree = rightRightRotation(tree);
+            }
+        } else if (cmp > 0) {    // 待删除的节点在"tree的右子树"中
+            tree.right = remove(tree.right, z);
+            // 删除节点后，若AVL树失去平衡，则进行相应的调节。
+            if (height(tree.left) - height(tree.right) == 2) {
+                AVLTreeNode<T> l = tree.left;
+                if (height(l.right) > height(l.left))
+                    tree = leftRightRotation(tree);
+                else
+                    tree = leftLeftRotation(tree);
+            }
+        } else {    // tree是对应要删除的节点。
+            // tree的左右孩子都非空
+            if ((tree.left != null) && (tree.right != null)) {
+                if (height(tree.left) > height(tree.right)) {
+                    // 如果tree的左子树比右子树高；
+                    // 则(01)找出tree的左子树中的最大节点
+                    //   (02)将该最大节点的值赋值给tree。
+                    //   (03)删除该最大节点。
+                    // 这类似于用"tree的左子树中最大节点"做"tree"的替身；
+                    // 采用这种方式的好处是：删除"tree的左子树中最大节点"之后，AVL树仍然是平衡的。
+                    AVLTreeNode<T> max = maximum(tree.left);
+                    tree.key = max.key;
+                    tree.left = remove(tree.left, max);
+                } else {
+                    // 如果tree的左子树不比右子树高(即它们相等，或右子树比左子树高1)
+                    // 则(01)找出tree的右子树中的最小节点
+                    //   (02)将该最小节点的值赋值给tree。
+                    //   (03)删除该最小节点。
+                    // 这类似于用"tree的右子树中最小节点"做"tree"的替身；
+                    // 采用这种方式的好处是：删除"tree的右子树中最小节点"之后，AVL树仍然是平衡的。
+                    AVLTreeNode<T> min = maximum(tree.right);
+                    tree.key = min.key;
+                    tree.right = remove(tree.right, min);
+                }
+            } else {
+                AVLTreeNode<T> tmp = tree;
+                tree = (tree.left != null) ? tree.left : tree.right;
+                tmp = null;
+            }
+        }
+
+        return tree;
+    }
+
+    public void remove(T key) {
+        AVLTreeNode<T> z;
+
+        if ((z = search(mRoot, key)) != null)
+            mRoot = remove(mRoot, z);
+    }
+
+    /*
+     * 销毁AVL树
+     */
+    private void destroy(AVLTreeNode<T> tree) {
+        if (tree == null)
+            return;
+
+        if (tree.left != null)
+            destroy(tree.left);
+        if (tree.right != null)
+            destroy(tree.right);
+
+        tree = null;
+    }
+
+    public void destroy() {
+        destroy(mRoot);
+    }
+
+    /*
+     * 打印"二叉查找树"
+     *
+     * key        -- 节点的键值
+     * direction  --  0，表示该节点是根节点;
+     *               -1，表示该节点是它的父结点的左孩子;
+     *                1，表示该节点是它的父结点的右孩子。
+     */
+    private void print(AVLTreeNode<T> tree, T key, int direction) {
+        if (tree != null) {
+            if (direction == 0)    // tree是根节点
+                System.out.printf("%2d is root\n", tree.key, key);
+            else                // tree是分支节点
+                System.out.printf("%2d is %2d's %6s child\n", tree.key, key, direction == 1 ? "right" : "left");
+
+            print(tree.left, tree.key, -1);
+            print(tree.right, tree.key, 1);
+        }
+    }
+
+    public void print() {
+        if (mRoot != null)
+            print(mRoot, mRoot.key, 0);
+    }
+
+    //-----------------------------打印树状结构--------------------------------
+    // 用于获得树的层数
+    public int getTreeDepth(AVLTreeNode<T> root) {
+        if (root == null) return 0;
+
+        int left = getTreeDepth(root.left);
+        int right = getTreeDepth(root.right);
+        return (1 + Math.max(left, right));
+    }
+
+    public void printTree() {
+        if (mRoot == null) System.out.println("EMPTY!");
+        // 得到树的深度
+        int treeDepth = getTreeDepth(mRoot);
+        SetGaps(treeDepth);
+
+        // 最后一行的宽度为2的（n - 1）次方乘3，再加1
+        // 作为整个二维数组的宽度
+        int arrayHeight = treeDepth * 2 - 1;
+        int arrayWidth = (2 << (treeDepth - 2)) * 6 + 1;
+        // 用一个字符串数组来存储每个位置应显示的元素
+        String[][] res = new String[arrayHeight][arrayWidth];
+        // 对数组进行初始化，默认为一个空格
+        for (int i = 0; i < arrayHeight; i++) {
+            for (int j = 0; j < arrayWidth; j++) {
+                res[i][j] = " ";
+            }
+        }
+
+
+        // 从根节点开始，递归处理整个树
+        // res[0][(arrayWidth + 1)/ 2] = (char)(root.val + '0');
+        writeArray(mRoot, 0, (arrayWidth / 2), res, treeDepth);
+
+        // 此时，已经将所有需要显示的元素储存到了二维数组中，将其拼接并打印即可
+        for (String[] line : res) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < line.length; i++) {
+                sb.append(line[i]);
+                if (line[i].length() > 1 && i <= line.length - 1) {
+                    i += line[i].length() > 4 ? 2 : line[i].length() - 1;
+                }
+            }
+            System.out.println(sb.toString());
+        }
+    }
+
+    int[] gaps = null;
+
+    void SetGaps(int treeDepth) {
+        gaps = new int[treeDepth];
+
+        for (int i = 0; i < treeDepth - 1; i++) {
+            int arrayWidth = (2 << (treeDepth - 2 - i)) * 6;
+            int rootlet = (arrayWidth / 2);
+            gaps[i] = rootlet / 4;
+        }
+
+        if (gaps.length >= 2) {
+            gaps[gaps.length - 2] = 2;
+        }
+        gaps[gaps.length - 1] = 1;
+    }
+
+    private void writeArray(AVLTreeNode<T> currNode, int rowIndex, int columnIndex, String[][] res, int treeDepth) {
+        // 保证输入的树不为空
+        if (currNode == null) return;
+        if (columnIndex > res[0].length - 1) return;
+
+        // 先将当前节点保存到二维数组中
+        res[rowIndex][columnIndex] = "[" + String.valueOf(currNode.key) + "]";
+
+        // 计算当前位于树的第几层
+        int currLevel = ((rowIndex + 1) / 2);
+        // 若到了最后一层，则返回
+        if (currLevel >= treeDepth) return;
+        // 计算当前行到下一行，每个元素之间的间隔（下一行的列索引与当前元素的列索引之间的间隔）
+        if (columnIndex < 3) return;
+
+        int gap = gaps[currLevel];
+
+        // 对左儿子进行判断，若有左儿子，则记录相应的"/"与左儿子的值
+        if (currNode.left != null) {
+            res[rowIndex + 1][columnIndex - gap] = "/";
+            int d = (gap == 2 ? 3 : gap * 2);
+            writeArray(currNode.left, rowIndex + 2, columnIndex - d, res, treeDepth);
+        }
+
+        // 对右儿子进行判断，若有右儿子，则记录相应的"\"与右儿子的值
+        if (currNode.right != null) {
+            res[rowIndex + 1][columnIndex + gap] = "\\";
+
+            int d = (gap == 2 ? 3 : gap * 2);
+            writeArray(currNode.right, rowIndex + 2, columnIndex + d, res, treeDepth);
+        }
+
+    }
+
+
+    private static int arr[] = {3, 2, 1, 4, 5, 6, 7, 16, 15, 14, 13, 12, 11, 10, 8, 9};
+
+    public static void main(String[] args) {
+        int i;
+        AVLTree<Integer> tree = new AVLTree<Integer>();
+
+        System.out.printf("== 原始数据: ");
+        System.out.println(Arrays.toString(arr));
+        for (i = 0; i < arr.length; i++) {
+            tree.insert(arr[i]);
+            System.out.printf("== 添加节点: %d\n", arr[i]);
+            System.out.printf("== 树的详细信息: \n");
+            tree.printTree();
+        }
+
+        System.out.printf("\n== 前序遍历: ");
+        tree.preOrder();
+
+        System.out.printf("\n== 中序遍历: ");
+        tree.inOrder();
+
+        System.out.printf("\n== 后序遍历: ");
+        tree.postOrder();
+        System.out.printf("\n");
+
+        System.out.printf("== 高度: %d\n", tree.height());
+        System.out.printf("== 最小值: %d\n", tree.minimum());
+        System.out.printf("== 最大值: %d\n", tree.maximum());
+        System.out.printf("== 树的详细信息: \n");
+        tree.print();
+
+        i = 8;
+        System.out.printf("\n== 删除根节点: %d", i);
+        tree.remove(i);
+
+        System.out.printf("\n== 高度: %d", tree.height());
+        System.out.printf("\n== 中序遍历: ");
+        tree.inOrder();
+        System.out.printf("\n== 树的详细信息: \n");
+        tree.print();
+
+        System.out.println("------------------------打印树状结构-----------------------");
+        tree.printTree();
+
+        // 销毁二叉树
+        tree.destroy();
+    }
+}
--- src/BinaryTree/RBTreeTest.java
+++ src/BinaryTree/RBTreeTest.java
@@ -1,5 +1,9 @@
 package BinaryTree;
 
+import utils.Utils;
+
+import java.util.HashSet;
+
 /**
  * Java 语言: 二叉查找树
  *
@@ -8,7 +12,8 @@ package BinaryTree;
  */
 public class RBTreeTest {
 
-    private static final int a[] = {10, 40, 30, 60, 90, 70, 20, 50, 80, 7, 9, 18, 17, 15, 13, 14, 8, 12, 21, 45, 68, 74, 88};
+    private static int count = 0;
+    private static Integer a[] = {10, 40, 30, 60, 90, 70, 20, 50, 80, 7, 9, 18, 17, 15, 13, 14, 8, 12, 21, 45, 68, 74, 88};
     private static final boolean mDebugInsert = true;    // "插入"动作的检测开关(false，关闭；true，打开)
     private static final boolean mDebugDelete = false;    // "删除"动作的检测开关(false，关闭；true，打开)
 
@@ -66,5 +71,88 @@ public class RBTreeTest {
 
         // 销毁二叉树
         tree.clear();
+
+        System.out.println("=================性能测试=========================");
+        count = 4600_0000;
+        System.out.println("-----------------------条数据准备时间:-----------------------:");
+        Utils.countTime(RBTreeTest::performanceTest);
+
+        System.out.println("=================插入性能测试=================");
+        System.out.println("-----平衡树AVLTree---" + a.length + "条数据插入时间:----------:");
+        Utils.countTime(RBTreeTest::AVLTreePerformanceTest);
+
+        System.out.println("-----红黑树RBTree----" + a.length + "条数据插入时间:----------:");
+        Utils.countTime(RBTreeTest::RBTreePerformanceTest);
+
+        System.out.println("=================查询性能测试=================");
+        System.out.println("-----平衡树AVLTree---" + a.length + "条数据查询时间:----------:");
+        Utils.countTime(RBTreeTest::AVLTreeSearchPerformanceTest);
+
+        System.out.println("-----红黑树RBTree----" + a.length + "条数据查询时间:----------:");
+        Utils.countTime(RBTreeTest::RBTreeSearchPerformanceTest);
+
+        System.out.println("=================删除性能测试=================");
+        System.out.println("-----平衡树AVLTree---" + a.length + "条数据删除时间:----------:");
+        Utils.countTime(RBTreeTest::AVLTreeDelPerformanceTest);
+
+        System.out.println("-----红黑树RBTree----" + a.length + "条数据删除时间:----------:");
+        Utils.countTime(RBTreeTest::RBTreeDelPerformanceTest);
+
+    }
+
+    static  int del[];
+
+    static void performanceTest() {
+        //  a = new int[count];
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < count-1; i++) {
+            set.add(Utils.getRandomInt(count * 20));
+        }
+
+        a = set.toArray(a);
+    }
+    static RBTree<Integer> RBTree = new RBTree<Integer>();
+    static void RBTreePerformanceTest() {
+        for (int i = 0; i < a.length; i++) {
+            RBTree.insert(a[i]);
+        }
+        // 销毁二叉树
+        //tree.clear();
+    }
+
+    static  AVLTree<Integer> AVLTree = new AVLTree<Integer>();
+    static void AVLTreePerformanceTest() {
+        for (int i = 0; i < a.length; i++) {
+            AVLTree.insert(a[i]);
+        }
+
+        // 销毁二叉树
+        //tree.destroy();
+    }
+
+    static void AVLTreeSearchPerformanceTest() {
+        for (int i = a.length - 1; i >= 0; i--) {
+            AVLTree.search(a[i]);
+        }
+    }
+
+    static void RBTreeSearchPerformanceTest() {
+        for (int i = a.length - 1; i >= 0; i--) {
+            RBTree.search(a[i]);
+        }
+    }
+
+    static void AVLTreeDelPerformanceTest() {
+        for (int i = a.length - 1; i >= 0; i--) {
+            AVLTree.remove(a[i]);
+        }
+    }
+
+    static void RBTreeDelPerformanceTest() {
+        for (int i = a.length - 1; i >= 0; i--) {
+            RBTree.remove(a[i]);
+        }
     }
 }
